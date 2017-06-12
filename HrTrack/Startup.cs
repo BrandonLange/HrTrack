a8fdd69d4a075d41dcd86fa0af7865e4c411a3cd
Brandon <brandon@drgt.net>
2017-06-12 08:38:01 +0200

    Added project to repo

---
+++ HrTrack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HrTrack.Startup))]
+namespace HrTrack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
