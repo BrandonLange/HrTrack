abacb19d7c40132466d7a2f430e4fb146a030969
Brandon <brandon@drgt.net>
2017-07-04 20:21:32 +0200

    Added template for forms

---
+++ app/index.js
@@ -0,0 +1,43 @@
+var SetupForm = function() {
+
+    var Initialise = function() {
+        Events();
+    }
+
+    var Events = function() {
+        $("#add-item").on("click", function() {
+
+            $("#policy-form").show();
+        })
+
+        $("#update-policy").on("click", function() {
+            var policyName = $("#policy-name").val();
+            var policyDesc = $("#policy-description").val();
+
+            var html = '<li class="collection-item blue darken-4">' + policyName + '</li>';
+
+            $(".collection").append(html);
+
+            var policyName = $("#policy-name").val("");
+            var policyDesc = $("#policy-description").val("");
+
+            $("#policy-form").hide();
+        })
+
+        $("#attach-document").on("click", function() {
+        	$("#file-upload").click();
+        })
+    }
+
+    return {
+        init: function() {
+            Initialise();
+        }
+    }
+}();
+
+$(document).ready(function() {
+    jQuery(function() {
+        SetupForm.init();
+    })
+})
